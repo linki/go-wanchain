558813bcac4871c149919bd6500b2e3df42ac65f
Martin Linkhorst <martin.linkhorst+github.com@posteo.de>
2020-02-01 15:56:58 +0100

    feat(gwan): unlock Keystore file via Google Cloud Secret Manager

--- cmd/gwan/usage.go
+++ cmd/gwan/usage.go
@@ -124,6 +124,9 @@ var AppHelpFlagGroups = []flagGroup{
 			utils.UnlockedAccountFlag,
 			utils.PasswordFileFlag,
 			utils.AwsKmsFlag,
+			utils.GCPProjectIDFlag,
+			utils.GCPSecretNameFlag,
+			utils.GCPSecretVersionFlag,
 		},
 	},
 	{
